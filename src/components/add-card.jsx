98ab5bce8ee5de86eda54a94b2a36e436a7ad1b5
Minseop Noh <nms2811@gmail.com>
2023-02-26 15:04:10 -0500

    minor design changes and data updates

---
+++ src/components/add-card.jsx
@@ -0,0 +1,4 @@
+const AddCard = () => {
+  <div></div>;
+};
+export default AddCard;
--- src/components/data.js
+++ src/components/data.js
@@ -1,28 +1,28 @@
 export const data = [
   {
     id: "1",
-    Task: "Create PR for the Task",
-    Due_Date: "25-May-2021",
+    Task: "Performance Management",
+    Due_Date: "1-Dec-2022",
   },
   {
     id: "2",
-    Task: "Fix Styling",
-    Due_Date: "26-May-2021",
+    Task: "Mobile Mockup",
+    Due_Date: "30-May-2022",
   },
   {
     id: "3",
-    Task: "Handle Api Changes",
-    Due_Date: "27-May-2021",
+    Task: "New Admin Console",
+    Due_Date: "27-Mar-2022",
   },
   {
     id: "4",
-    Task: "Blog on new features",
-    Due_Date: "23-Aug-2021",
+    Task: "Code Reviews",
+    Due_Date: "14-Aug-2022",
   },
   {
     id: "5",
-    Task: "Call with Backend Team",
-    Due_Date: "05-Jan-2021",
+    Task: "SEO Plan",
+    Due_Date: "02-Feb-2023",
   },
 ];
 
